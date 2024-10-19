f2b36a022aa97e6d495751291336d836e0e8860f
Olha <oyashnaya88@gmail.com>
2024-10-20 00:40:59 +0300

    markup

---
+++ src/js/1-timer.js
@@ -0,0 +1,10 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
