737de7129c38f7081cf174c389f462e64786c9e4
Olha <oyashnaya88@gmail.com>
2024-10-21 00:18:32 +0300

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,10 +1,81 @@
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
-
+import 'flatpickr/dist/flatpickr.min.css';
 
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
+
+let userSelectedDate = '';
+
+const dateInput = document.querySelector('#datetime-picker');
+const timerBtn = document.querySelector('.timer-btn');
+const days = document.querySelector('.value[data-days]');
+const hours = document.querySelector('.value[data-hours]');
+const minutes = document.querySelector('.value[data-minutes]');
+const seconds = document.querySelector('.value[data-seconds]');
+
+timerBtn.disable = true;
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0] <= Date.now()) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    } else {
+      timerBtn.disable = false;
+      userSelectedDate = selectedDates[0];
+    }
+  },
+};
+
+flatpickr(dateInput, options);
+
+timerBtn.addEventListener('click', handleClick);
+
+
+let intervalId = 0;
+
+function handleClick() {
+  intervalId = setInterval(createTimer, 1000);
+  timerBtn.disabled = true;
+  dateInput.disabled = true;
+}
+
+  
+
+function clearTimer() {
+    clearInterval(intervalId);
+    timerBtn.disabled = true;
+  }
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
